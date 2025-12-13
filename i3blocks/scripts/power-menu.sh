f453664abafb0447073a35aba3c1421951335b8b
George Thomas <geothomasgr@protonmail.com>
2025-12-13 17:12:17 +0200

    Create power-menu.sh

---
+++ i3blocks/scripts/power-menu.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+choice=$(printf "sleep\nreboot\npoweroff\ncancel" | rofi -dmenu -p "POWER")
+
+case "$choice" in
+    sleep)
+        systemctl suspend
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    poweroff)
+        confirm=$(printf "NO\nYES" | rofi -dmenu -p "Confirm Power Off")
+        [ "$confirm" = "YES" ] && systemctl poweroff
+        ;;
+    *)
+        exit 0
+        ;;
+esac
